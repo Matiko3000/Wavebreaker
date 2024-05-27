f64c82e320bf6d3ffa6c936a497892d2a54d11f5
Matiko3000 <matiko3000@gmail.com>
2024-05-28 00:56:24 +0200

    Added Horizontal Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,36 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 2f;
 
     private Vector2 input;
+    
+    Rigidbody2D rb;
+    Animator animator;
 
-    void Update()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    void OnMove(InputValue value)
+    void FixedUpdate()
+    {
+        moveHorizontally();
+    }
+
+    void OnMove(InputValue value) //get the input vector
     {
         input = value.Get<Vector2>();
-        Debug.Log(input);
+    }
+
+    void moveHorizontally()//physically move and animate
+    {
+        rb.velocity = new Vector2(input.x * moveSpeed, rb.velocity.y);
+
+        if (Mathf.Abs(input.x) > Mathf.Epsilon)
+        {
+            animator.SetBool("isRunning", true);
+            if (input.x < 0) transform.rotation = Quaternion.Euler(0,180,0); //flipping the sprite based on the direction
+            else transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else animator.SetBool("isRunning", false);
     }
 }
