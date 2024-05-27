758a4728b33469d0c3ca3e87f8d9d48352db681a
Matiko3000 <matiko3000@gmail.com>
2024-05-28 00:08:01 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+
+    private Vector2 input;
+
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        input = value.Get<Vector2>();
+        Debug.Log(input);//test
+    }
+}
