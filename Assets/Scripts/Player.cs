c9f7253a156a925eaa4971a89a30562b1b79959c
Matiko3000 <matiko3000@gmail.com>
2024-05-29 00:04:09 +0200

    PlayerMovement finished + enemy object added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,14 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] int jumpAmount = 1;
+    [SerializeField] float fallingScale = 1;
 
     private Vector2 input;
-    
+    private int currentJumps = 0;
+
+    BoxCollider2D groundCheck;
     Rigidbody2D rb;
     Animator animator;
 
@@ -16,12 +21,16 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        groundCheck = GetComponent<BoxCollider2D>();
     }
 
     void FixedUpdate()
     {
         moveHorizontally();
+        moveVertically();
     }
+
+    #region moving
 
     void OnMove(InputValue value) //get the input vector
     {
@@ -40,4 +49,40 @@ public class Player : MonoBehaviour
         }
         else animator.SetBool("isRunning", false);
     }
+
+    #endregion
+    #region jumping
+
+    void OnJump(InputValue value) //check if player should be able to jump
+    {
+        if (currentJumps < jumpAmount)
+        {
+            currentJumps++;
+            Jump();
+        }
+        else if (groundCheck.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            currentJumps = 1;
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity += new Vector2(0f, jumpForce + (-rb.velocity.y)); //add the negative y velocity for the double jump to work properly
+    }
+
+    void moveVertically() //take care of the animations and add the falling scale
+    {
+        if (rb.velocity.y > 0.1) { animator.SetFloat("velocity.y", 1); animator.SetBool("isJumping", true); }
+        else if (rb.velocity.y < -0.1) { animator.SetFloat("velocity.y", -1); animator.SetBool("isJumping", true); }
+        else { animator.SetFloat("velocity.y", 0); animator.SetBool("isJumping", false); }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity -= new Vector2(0f, fallingScale);
+        }
+    }
+
+    #endregion
 }
